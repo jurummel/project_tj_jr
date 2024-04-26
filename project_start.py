5b2fab8b09ef7526ce4f8f7ab9f9d3b9e247fdca
jurummel <julian.rummel98@gmail.com>
2024-04-27 00:06:42 +0200

    parser

--- project_start.py
+++ project_start.py
@@ -1,5 +1,36 @@
+import json
+
 import sklearn as sk
 import pandas as pd
 import numpy as np
 import django as dj
+import json as js
+
+movies = pd.read_csv("/Users/julian/Documents/project_new_TJ/pythonProject/Data/movies_metadata.csv")
+movies = movies[movies["status"]=="Released"]
+df_name_genre = movies[["title", "genres"]]
+df_name_genre = df_name_genre[df_name_genre["genres"].str.len()>2]
+
+genres_id_classification = {}
+help_list=[]
+idents = []
+
+#pd.json_normalize(df_name_genre["genres"])
+#print(genres_id_classification)
+
+for genre_list in df_name_genre["genres"]:
+    print(genre_list)
+    for char in range(0, len(genre_list)-2):
+        if genre_list[char] == "i" and genre_list[char+1] == "d" and genre_list[char+2] == "'":
+            char += 5
+            while genre_list[char] != ',':
+                print(genre_list[char])
+                help_list.append(genre_list[char])
+                idents.append(' '.join(help_list))
+                help_list = []
+                char += 1
+        else:
+           pass
+
+print(idents)
 
