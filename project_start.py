46f2167ab650cca6dc4518db94db153f86448d80
jurummel <julian.rummel98@gmail.com>
2024-04-27 00:28:03 +0200

    parser done

--- project_start.py
+++ project_start.py
@@ -11,12 +11,11 @@ movies = movies[movies["status"]=="Released"]
 df_name_genre = movies[["title", "genres"]]
 df_name_genre = df_name_genre[df_name_genre["genres"].str.len()>2]
 
+genres_id_classification_preprocess = {}
 genres_id_classification = {}
 help_list=[]
 idents = []
-
-#pd.json_normalize(df_name_genre["genres"])
-#print(genres_id_classification)
+names = []
 
 for genre_list in df_name_genre["genres"]:
     print(genre_list)
@@ -24,13 +23,37 @@ for genre_list in df_name_genre["genres"]:
         if genre_list[char] == "i" and genre_list[char+1] == "d" and genre_list[char+2] == "'":
             char += 5
             while genre_list[char] != ',':
-                print(genre_list[char])
+                #print(genre_list[char])
                 help_list.append(genre_list[char])
-                idents.append(' '.join(help_list))
-                help_list = []
                 char += 1
+
+            idents.append(''.join(help_list))
+            help_list = []
+
+            char += 11
+            while genre_list[char] != '\'':
+                help_list.append(genre_list[char])
+                char += 1
+
+            names.append(''.join(help_list))
+            help_list = []
+
         else:
            pass
 
-print(idents)
+for elem in idents:
+    elem = int(elem)
 
+for key in idents:
+    for value in names:
+        genres_id_classification_preprocess[key] = value
+        names.remove(value)
+        break
+
+temp = []
+for key, val in genres_id_classification_preprocess.items():
+    if val not in temp:
+        temp.append(val)
+        genres_id_classification[key] = val
+
+print(genres_id_classification)
